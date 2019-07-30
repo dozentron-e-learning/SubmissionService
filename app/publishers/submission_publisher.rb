c4acf9dc28a4ab9e30e5d1eff3be928b2e0cf7fb
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-30 15:08:20 +0200

    add exercise id to published message

--- app/publishers/submission_publisher.rb
+++ app/publishers/submission_publisher.rb
@@ -2,12 +2,12 @@ class SubmissionPublisher < ActiveBunny::Publisher
   def create(submission)
     Rails.logger.info "Send Message about new Submission: #{submission.id.to_s}"
     #TODO Generate token to use in controller
-    publish({id: submission.id.to_s, validation_token: ""}.to_json)
+    publish({id: submission.id.to_s, exercise_id: submission.exercise_id.to_s, validation_token: ""}.to_json)
   end
 
   def update(submission)
     Rails.logger.info "Send Message about updated Submission: #{submission.id.to_s}"
     #TODO Generate token to use in controller
-    publish({id: submission.id.to_s, validation_token: ""}.to_json)
+    publish({id: submission.id.to_s, exercise_id: submission.exercise_id.to_s, validation_token: ""}.to_json)
   end
 end
