c50c017bbd5b17beff5b52c2f6075b63816dc42a
Dominic Althaus <dominic.althaus@mni.thm.de>
2019-07-30 15:08:06 +0200

    add attributes

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,7 +2,7 @@ class Submission
   include Mongoid::Document
 
   VALIDATION_NOT_PERFORMED = :not_performed
-  VALIDATION_SUCCEEDED = :succeeded
+  VALIDATION_SUCCEEDED = :success
   VALIDATION_FAILED = :failed
 
   after_create { |obj| SubmissionPublisher.new.create obj }
@@ -12,16 +12,22 @@ class Submission
     exercise_id
     file
     validation_status
+    file_validation_error
+    general_validation_error
+    general_validation_error_details
   ].freeze
 
   mount_uploader :file, SubmissionUploader
 
-  field :validation_status, type: Boolean, default: VALIDATION_NOT_PERFORMED
+  field :validation_status, type: Symbol, default: VALIDATION_NOT_PERFORMED
   field :exercise_id, type: BSON::ObjectId
+  field :file_validation_error, type: String
+  field :general_validation_error, type: String
+  field :general_validation_error_details, type: String
 
   validates :file, presence: true
 
   def validation_success?
-    self.validation_status == VALIDATION_SUCCEEDED
+    self.validation_status == VALIDATION_SUCCEEDED || self.validation_status == VALIDATION_SUCCEEDED.to_s
   end
 end
